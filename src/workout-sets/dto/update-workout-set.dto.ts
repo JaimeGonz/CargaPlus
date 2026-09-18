154969a32fa5b277b87b79400b6c9cebaa2e809e
Jaime González <valdoc7@gmail.com>
2026-09-17 22:00:13 -0600

    feat: allow marking a workout set as completed

--- src/workout-sets/dto/update-workout-set.dto.ts
+++ src/workout-sets/dto/update-workout-set.dto.ts
@@ -1,4 +1,10 @@
-import { PartialType } from '@nestjs/swagger';
+import { ApiPropertyOptional, PartialType } from '@nestjs/swagger';
 import { CreateWorkoutSetDto } from './create-workout-set.dto';
+import { IsBoolean, IsOptional } from 'class-validator';
 
-export class UpdateWorkoutSetDto extends PartialType(CreateWorkoutSetDto) {}
+export class UpdateWorkoutSetDto extends PartialType(CreateWorkoutSetDto) {
+  @IsOptional()
+  @IsBoolean()
+  @ApiPropertyOptional({ example: true })
+  isCompleted?: boolean;
+}
